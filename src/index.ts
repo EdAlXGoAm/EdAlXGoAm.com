2938d9758beb928a46b4d2af74821432fe37128f
EdAlXGoAm <edalxgoam@gmail.com>
2023-07-04 15:54:17 -0600

    Finish eslint y prettier

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,6 @@ const app = express();
 app.get('/', (req, res) => {
     res.json({ message: 'Hello World' });
 });
-app.use('/', indexRouter)
+app.use('/', indexRouter);
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
