902c462f0c73f86abc49b79ea46f641ecf25fb79
EdAlXGoAm <edalxgoam@gmail.com>
2023-07-07 20:02:26 -0600

    Adding ejs and react

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
-
+import path from 'path';
 import indexRouter from '@routes/index.route';
 
 const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello World' });
-});
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Settings for Views
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use('/', indexRouter);
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+app.listen(port, () => console.log('Server is running on port 3000'));
--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -2,6 +2,10 @@ import {Router} from 'express';
 
 const router = Router();
 
+router.get('/', (req, res) => {
+    res.render('index', {title: 'EdAlXGoAm'});
+});
+
 router.get('/api', (req, res) => {
     res.send('Welcome to my API!');
 });
